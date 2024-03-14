09b7b47aa02527e67acaf932e6b183442a78fa53
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-03-14 08:56:21 -0300

    Adds new investment and registered investment pages

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('investments/new/', views.new_investment, name='new_investment'),
+    path('investments/registered/', views.registered_investment, name='registered_investment'),
 ]
--- app/invista_me/views.py
+++ app/invista_me/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
+def home(_):
     return HttpResponse('Home Page!!')
 
 def about(request):
@@ -15,3 +15,16 @@ def about(request):
         'job': 'Developer',
     }
     return render(request, 'hero/about.html', user)
+
+# Investments views
+
+def new_investment(request):
+    return render(request, 'investments/new_investment.html')
+
+def registered_investment(request):
+    investment = {
+        'name': request.POST.get('name'),
+        'type': request.POST.get('type'),
+        'value': request.POST.get('value')
+    }
+    return render(request, 'investments/registered_investment.html', investment)

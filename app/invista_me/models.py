b00d8a967a944ad7c94e9142878e395464d5a50c
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-03-14 09:05:18 -0300

    Creates the ivestment model

--- app/invista_me/models.py
+++ app/invista_me/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class   Investment(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    value = models.DecimalField(max_digits=15, decimal_places=2)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
